4a1fabbebdbdf6aba70e9cae3d4173022931832f
Devsethi3 <devsethi4522@gmail.com>
2024-08-30 21:26:39 +0530

    Optimize content structure and responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import About from "@/components/About";
 import HeroSection from "@/components/HeroSection";
 import Navbar from "@/components/Navbar";
 import Services from "@/components/Services";
-import { Button } from "@/components/ui/button";
 import React from "react";
 
 const page = () => {
