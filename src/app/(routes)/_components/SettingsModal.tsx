e784446ad0e0426b561dbae0b18aa95d26c034e7
Devsethi3 <devsethi4522@gmail.com>
2024-09-07 16:54:02 +0530

    Update SettingsModal.tsx

--- src/app/(routes)/_components/SettingsModal.tsx
+++ src/app/(routes)/_components/SettingsModal.tsx
@@ -114,7 +114,7 @@ const SettingsModal: React.FC<SettingsModalProps> = ({
         photoURL = await getDownloadURL(storageRef);
       }
 
-      //    
+          
       await updateProfile(authUser, {
         displayName: newDisplayName,
         photoURL: photoURL,
